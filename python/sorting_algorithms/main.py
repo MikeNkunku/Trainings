7dab14850ba96ac5c72006833ff0abcb369db54f
MikeNkunku <mikenkunku@gmail.com>
2016-06-29 06:59:14 +0200

    Added main file

---
+++ python/sorting_algorithms/main.py
@@ -0,0 +1,19 @@
+from bubble_sort import BubbleSort as BS
+# from insertion_sort import InsertionSort as IS
+# from merge_sort import MergeSort as MS
+# from quick_sort import QuickSort as QS
+# from selection_sort import SelectionSort as SelS
+# from shell_sort import ShellSort as ShS
+from os import _exit as exit
+from sys import exec_info
+
+if __name__ != '__main__':
+	print("Must rename file into 'main.py'!")
+	exit(1)
+
+try:
+	
+
+
+except Exception, e:
+	raise e
