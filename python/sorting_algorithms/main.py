6463311177cc8f29f057f52e0fdf1e5c3d9b6f04
MikeNkunku <mikenkunku@gmail.com>
2016-07-09 09:55:52 +0200

    Defined couple of functions/options to be used in the program menu + Cleaned code

--- python/sorting_algorithms/main.py
+++ python/sorting_algorithms/main.py
@@ -1,19 +1,67 @@
-from bubble_sort import BubbleSort as BS
-# from insertion_sort import InsertionSort as IS
-# from merge_sort import MergeSort as MS
-# from quick_sort import QuickSort as QS
-# from selection_sort import SelectionSort as SelS
-# from shell_sort import ShellSort as ShS
+import sorting_algorithms
+from sort_handler import SortHandler as SH
+
 from os import _exit as exit
-from sys import exec_info
+from sys import exc_info
 
 if __name__ != '__main__':
 	print("Must rename file into 'main.py'!")
 	exit(1)
 
-try:
-	
+options = {
+	1: "Put list",
+	2: "Display list",
+	3: "Sort list",
+	4: "Exit"
+}
 
+def put_list(sh_inst, a_list = None):
+	while (not(isinstance(a_list, list))):
+		print("Insert your list:")
+		a_list = eval(raw_input())
+		# print(sh_inst)
+
+	sh_inst.a_list = a_list
+
+def display_list(sh_inst):
+	print "\nA list must be defined first!" if (sh_inst.a_list == None) else ("List: %s" % str(sh_inst.a_list))
+
+def sort_list(sh_inst):
+	pass
+
+def quit(sh_inst):
+	print("\nFarewell!\n\n"
+		"Quitting...")
+
+actions = {
+	1: put_list,
+	2: display_list,
+	3: sort_list,
+	4: quit
+}
+
+def print_menu():
+	print('\n')
+	for o in options:
+		print("%i - %s" % (o, options[o]))
+
+def get_user_choice():
+	user_choice = 0
+
+	while ((user_choice < 1) or (user_choice > len(options))):
+		print_menu()
+		user_choice = int(raw_input())
+
+	return user_choice
+
+try:
+	# user_choice = None
+	# exit_choice = [k for k, v in options.items() if v == "Exit"][0]
+
+	# while (user_choice != exit_choice):
+	# 	user_choice = get_user_choice()
+
+	# 	actions[user_choice](sh_instance)
 
 except Exception, e:
 	raise e
