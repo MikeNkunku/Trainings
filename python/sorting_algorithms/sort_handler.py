a905468ccb28b6ad14a79d102c21fe4a02391d26
MikeNkunku <mikenkunku@gmail.com>
2016-07-09 09:41:41 +0200

    Removed use of Singleton DP

--- python/sorting_algorithms/sort_handler.py
+++ python/sorting_algorithms/sort_handler.py
@@ -1,21 +1,5 @@
 class SortHandler():
-	class __Singleton: # private attribute
-		def __init__():
-			self.val = None
-
-		def __str__(self):
-			return `self` + self.val
-
-	def __new__(c):
-		if (not(Singleton.instance)):
-			Singleton.instance = Singleton.__Singleton()
-
-	def __getattr__(self, attr):
-		return getattr(self.instance, attr)
-
-	def __setattr__(self, attr, val):
-		return setattr(self.instance, attr, val)
-
-	instance = None
-	a_list = None
-	sort_class = None
+	def __init__(self):
+		self.val = None
+		self.a_list = None
+		self.sort_class = None
