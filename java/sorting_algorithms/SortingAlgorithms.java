8f28c596d37fdc20e0585fad17c76158887a5dd2
MikeNkunku <mikenkunku@gmail.com>
2016-07-13 06:27:40 +0200

    Added options + Updated types + Added Scanner attribute to class + Implemented most options

--- java/sorting_algorithms/SortingAlgorithms.java
+++ java/sorting_algorithms/SortingAlgorithms.java
@@ -1,20 +1,25 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedHashMap;
+import java.util.HashMap;
 import java.util.Collections;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class SortingAlgorithms {
 	private SortClass sc;
-	private ArrayList al;
+	private ArrayList<Double> al;
+	private final Scanner in = new Scanner(System.in);
 	private final LinkedHashMap<String, Integer> options = new LinkedHashMap<String, Integer>() {{
 		put("Put list", 1);
 		put("Display list", 2);
-		put("Exit", 3);
+		put("Put sorting method", 3);
+		put("Sort list", 4);
+		put("Exit", 5);
 	}};
 
-	public SortingAlgorithms(SortClass sc, ArrayList al) {
+	public SortingAlgorithms(SortClass sc, ArrayList<Double> al) {
 		this.sc = sc;
 		this.al = al;
 	}
@@ -27,7 +32,7 @@ public class SortingAlgorithms {
 		this(sc, null);
 	}
 
-	public SortingAlgorithms(ArrayList al) {
+	public SortingAlgorithms(ArrayList<Double> al) {
 		this(null, al);
 	}
 
@@ -39,11 +44,11 @@ public class SortingAlgorithms {
 		this.sc = sc;
 	}
 
-	public ArrayList getList() {
+	public ArrayList<Double> getList() {
 		return this.al;
 	}
 
-	public void setList(ArrayList al) {
+	public void setList(ArrayList<Double> al) {
 		this.al = al;
 	}
 
@@ -51,13 +56,18 @@ public class SortingAlgorithms {
 		return this.options;
 	}
 
+	public Scanner getScanner() {
+		return this.in;
+	}
+
 	public void displayList() {
-		this.sc.display(this.al);
+		System.out.println(this.getList());
 	}
 
 	public void displayMenu() {
 		LinkedHashMap<String, Integer> ht = this.getOptions();
 
+		System.out.println();
 		for(String key: ht.keySet()) {
 			System.out.println(ht.get(key) + " - " + key);
 		}
@@ -65,11 +75,10 @@ public class SortingAlgorithms {
 
 	public int getUserChoice() {
 		int userChoice = 0;
-		Scanner in = new Scanner(System.in);
 
 		while((userChoice < Collections.min(this.getOptions().values())) || (userChoice > Collections.max(this.getOptions().values()))) {
 			this.displayMenu();
-			userChoice = in.nextInt();
+			userChoice = this.getScanner().nextInt();
 		}
 
 		return userChoice;
@@ -90,15 +99,52 @@ public class SortingAlgorithms {
 		System.out.println('\n');
 	}
 
+	public void putList() {
+		this.setList(new ArrayList<Double>());
+
+		System.out.print("Enter your elements: ");
+		while (this.getScanner().hasNextDouble()) {
+			this.getList().add(this.getScanner().nextDouble());
+		}
+		this.getScanner().next();
+	}
+
+	public void putSortClass() {
+		this.setSortClass(new BubbleSort());
+	}
+
+	public void sortList() {
+		if (this.getSortClass() == null) {
+			System.out.println("A SortClass must be defined first!");
+		} else {
+			this.setList(this.getSortClass().sort(this.getList()));
+		}
+	}
+
+	public void quit() {
+		System.out.println();
+		this.printStars(15);
+		System.out.print(" See ya ");
+		this.printStars(15);
+		System.out.println();
+	}
+
 	public static void main(String[] args) {
 		SortingAlgorithms sa = new SortingAlgorithms();
 		MergeSort ms = new MergeSort();
-		// ArrayList<int> al = new ArrayList<int>(Arrays.asList(1, 3));
+		HashMap<Integer, Runnable> commands = new HashMap<Integer, Runnable>() {{
+			put(1, () -> sa.putList());
+			put(2, () -> sa.displayList());
+			put(3, () -> sa.putSortClass());
+			put(4, () -> sa.sortList());
+			put(5, () -> sa.quit());
+		}};
 		int userChoice = 0;
 
 		sa.printTitle();
 		while (userChoice != sa.getOptions().get("Exit")) {
 			userChoice = sa.getUserChoice();
+			commands.get(userChoice).run();
 		}
 	}
 }
