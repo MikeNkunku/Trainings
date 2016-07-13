1108ce8be84e4e2f40306f069c53aa128d370a20
MikeNkunku <mikenkunku@gmail.com>
2016-07-13 06:26:09 +0200

    Updated types

--- java/sorting_algorithms/SortClass.java
+++ java/sorting_algorithms/SortClass.java
@@ -1,11 +1,11 @@
 import java.util.ArrayList;
 
 interface ISortingAlgorithm {
-	ArrayList sort(ArrayList al);
+	ArrayList<Double> sort(ArrayList<Double> al);
 }
 
 public abstract class SortClass implements ISortingAlgorithm {
-	public void display(ArrayList al) {
+	public void display(ArrayList<Double> al) {
 		System.out.println(al);
 	}
 }
