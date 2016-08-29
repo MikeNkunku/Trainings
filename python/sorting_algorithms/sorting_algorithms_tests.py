124046923601a646ba299b3408c3eb311b38bc12
MikeNkunku <mikenkunku@gmail.com>
2016-08-29 22:31:32 +0200

    Added setUp and tearDown methods

--- python/sorting_algorithms/sorting_algorithms_tests.py
+++ python/sorting_algorithms/sorting_algorithms_tests.py
@@ -2,4 +2,9 @@ import unittest as ut
 import sorting_algorithms
 
 class SortingAlgorithmTestCase(ut.TestCase):
-	pass
+	def setUp(self):
+		self.a_list = [0, 3, 2 , 4.9, 100, 54, 505]
+
+	def tearDown(self):
+		self.a_list.dispose()
+		self.a_list = None
